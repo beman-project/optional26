e4366f74f5cb7f9239ffef1eb01ba1307fc9947b
Darius Neatu <dariusn@adobe.com>
2024-06-14 10:01:41 +0300

    Remove enable_view<optional<T&>> specialization: optional<T> is already always a view

--- include/Beman/Optional26/optional.hpp
+++ include/Beman/Optional26/optional.hpp
@@ -209,13 +209,6 @@ namespace std {
 template <typename T>
 inline constexpr bool ranges::enable_view<beman::optional::optional<T>> = true;
 
-// Since ${PAPER_NUMBER}: ${PAPER_TITLE}.
-// Note: P3168 and P2988 may have different flows inside LEWG/LWG.
-// Implementation of the range support for optional<T&> reflects P3168R1 for now.
-// [optional.iterators], iterator support
-template <typename T>
-inline constexpr bool ranges::enable_view<beman::optional::optional<T&>> = true;
-
 // TODO: document why this is needed.
 template <typename T>
 inline constexpr bool ranges::enable_borrowed_range<beman::optional::optional<T*>> = true;
