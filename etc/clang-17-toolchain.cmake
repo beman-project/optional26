81cb9fc899d4ef1deb15353ee0f14825352535ac
Steve Downey <sdowney2@bloomberg.net>
2024-06-18 17:05:44 -0400

    Use libstdc++ with clang-17 because libc++-17 doesn't have `join`
    libc++ hadn't implemented join as of 17, use libstdc++ instead.

--- etc/clang-17-toolchain.cmake
+++ etc/clang-17-toolchain.cmake
@@ -4,3 +4,7 @@ set(CMAKE_C_COMPILER clang-17)
 set(CMAKE_CXX_COMPILER clang++-17)
 
 include("${CMAKE_CURRENT_LIST_DIR}/clang-flags.cmake")
+
+set(CMAKE_CXX_FLAGS
+   "-stdlib=libstdc++ -Wall -Wextra "
+CACHE STRING "CXX_FLAGS" FORCE)
