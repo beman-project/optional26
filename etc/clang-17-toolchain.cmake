81c17e6fb9d5e47ee01b8227bdf64f333d105516
Steve Downey <sdowney@gmail.com>
2024-06-19 18:38:22 -0400

    Revert attempt to use libstdc++ with clang-170003
    A different set of problems with googletest HEAD arises.

--- etc/clang-17-toolchain.cmake
+++ etc/clang-17-toolchain.cmake
@@ -4,7 +4,3 @@ set(CMAKE_C_COMPILER clang-17)
 set(CMAKE_CXX_COMPILER clang++-17)
 
 include("${CMAKE_CURRENT_LIST_DIR}/clang-flags.cmake")
-
-set(CMAKE_CXX_FLAGS
-   "-stdlib=libstdc++ -Wall -Wextra "
-CACHE STRING "CXX_FLAGS" FORCE)
