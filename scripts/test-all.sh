2e75419bd0af496f91b0253b5ce3c5caae96098c
Darius Neatu <dariusn@adobe.com>
2024-06-27 22:48:28 +0300

    Tweaks in test-all.sh

--- scripts/test-all.sh
+++ scripts/test-all.sh
@@ -2,19 +2,89 @@
 
 # SPDX-License-Identifier: Apache-2.0 WITH LLVM-exception
 
-# Test all configurations: gcc-14, gcc-13, clang-18, clang-17.
+# Run tests on all known toolchains.
 # Expected to be run from the root of the project.
 # Expected all configurations to be present in the CMakeLists.txt file and installed on the system.
-# Usage: ./scripts/test-all.sh
 
 set -e
-set -x
 
-rm -rf .build
+# Toolchains to test.
+TOOLCHAINS=(
+    gcc-14
+    gcc-13
+    clang-19
+    clang-18
+    clang-17
+)
 
-cmake --workflow --preset gcc-14   &> /dev/null && echo "passed" || echo "gcc-14 test failed."
-cmake --workflow --preset gcc-13   &> /dev/null && echo "passed" || echo "gcc-13 test failed."
-cmake --workflow --preset clang-18 &> /dev/null && echo "passed" || echo "clang-18 test failed."
-cmake --workflow --preset clang-17 &> /dev/null && echo "passed" || echo "clang-17 test failed."
+# Build directory.
+BUILD_DIR=".build"
 
-echo "All tests passed."
+# Print usage information.
+function usage() {
+    echo "Usage: $0"
+    echo "  -h, --help: Display help."
+    echo "  -v, --verbose: Display verbose output."
+    echo "  -f, --fresh: Run tests on a fresh build directory."
+}
+
+# Parse command line arguments.
+function parse_args() {
+    while [[ $# -gt 0 ]]; do
+        case $1 in
+            -h|--help)
+                usage
+                exit 0
+                ;;
+            -v|--verbose)
+                VERBOSE=true
+                shift
+                ;;
+            -f|--fresh)
+                FRESH=true
+                shift
+                ;;
+            *)
+                echo "Unknown option: $1"
+                usage
+                exit 1
+                ;;
+        esac
+    done
+
+}
+
+# Run tests with a specific toolchain.
+function test_with_toolchain() {
+    toolchain=$1
+    echo "Testing with ${toolchain}..."
+
+    cmd="cmake --workflow --preset ${toolchain}"
+    if [ ! "$VERBOSE" ]; then
+        cmd+="&> /dev/null"
+    fi
+
+    if ! eval "${cmd}"; then
+        echo " -> failed"
+        exit 1
+    else
+        echo " -> passed"
+    fi
+}
+
+# Run tests with all toolchains.
+function test_all_toolchains() {
+    if [ "$FRESH" ]; then
+        echo " -> cleaning build directory"
+        rm -rf "${BUILD_DIR}"
+    fi
+
+    for toolchain in "${TOOLCHAINS[@]}"; do
+        test_with_toolchain "${toolchain}"
+    done
+
+    echo "All tests passed on all toolchains."
+}
+
+parse_args "$@"
+test_all_toolchains
