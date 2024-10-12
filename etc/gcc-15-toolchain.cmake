467bdb2ca9a2a0692c4790fd4b80184a4146d6b9
Steve Downey <sdowney2@bloomberg.net>
2024-10-11 22:54:45 -0400

    Make maybe-uninitialized no warn in Ason
    GCC is flase positive wanring on this in Anan mode.

--- etc/gcc-15-toolchain.cmake
+++ etc/gcc-15-toolchain.cmake
@@ -4,3 +4,7 @@ include("${CMAKE_CURRENT_LIST_DIR}/gcc-flags.cmake")
 
 set(CMAKE_C_COMPILER gcc-15)
 set(CMAKE_CXX_COMPILER g++-15)
+
+set(CMAKE_CXX_FLAGS_ASAN
+  "${CMAKE_CXX_FLAGS_ASAN} -Wno-maybe-uninitialized"
+  CACHE STRING "C++ ASAN Flags" FORCE)
